8899599c809332b7c793a1f9383b268fd6d24d23
tai <nguyentientai10@gmail.com>
2022-05-28 19:05:24 +0700

    add frontend

--- frontend/src/useContext/GlobalState.js
+++ frontend/src/useContext/GlobalState.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://messagesfree.herokuapp.com";
 export const socket = io(SOCKET_URL);
 export const Store = createContext();
 
--- frontend/src/utils/ApiRoutes.js
+++ frontend/src/utils/ApiRoutes.js
@@ -1,5 +1,6 @@
 //* Port
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://messagesfree.herokuapp.com";
 
 //* Authentication
 //!register
