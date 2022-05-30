bfaaccc928b281f5937d9a623147cab1fa11b737
tai <nguyentientai10@gmail.com>
2022-05-30 17:16:41 +0700

    fix

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,22 @@ const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const path = require("path");
+const compression = require("compression");
 app.enable("trust proxy");
 const bodyParser = require("body-parser");
 app.use(express.json());
+app.use(
+  compression({
+    level: 6,
+    threshold: 100 * 1000,
+    filter: (req, res) => {
+      if (req.headers["x-no-compression"]) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  })
+);
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -33,5 +46,10 @@ app.use("/api/group", Groups);
 app.use("/api/message", Messages);
 //!upload
 app.use("/api/upload", upload);
-//!Middleware for error
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 module.exports = app;
