3cc0917d008236e6ca336d74a9047db5e35832dc
tai <nguyentientai10@gmail.com>
2022-05-30 17:10:44 +0700

    fix

--- frontend/src/useContext/GlobalState.js
+++ frontend/src/useContext/GlobalState.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 // const SOCKET_URL = "https://messagesfree.herokuapp.com";
-const SOCKET_URL = "http://localhost:5000";
+const SOCKET_URL = "https://messagesfree.herokuapp.com";
 export const socket = io(SOCKET_URL);
 export const Store = createContext();
 
